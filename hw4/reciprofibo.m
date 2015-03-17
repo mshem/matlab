6bfab408eb3747d97d92111255cce6bfba6cd953
Michael Shemesh <mshem@brandeis.edu>
2015-03-17 12:47:43 -0400

    added comments

--- hw4/reciprofibo.m
+++ hw4/reciprofibo.m
@@ -1,14 +1,16 @@
+% Author: Michael Shemesh   
+
 function r = reciprofibo(n)
-r=2;
-a=1;
-b=1;
-c=0;
-for i=2:n
-    
-   r=r+1/(b+a);
-   c=a;
-   a=a+b;
-   b=c;
-end
+    r=2; %fibo index
+    %base cases
+    a=1;
+    b=1;
+    c=0;
+    for i=2:n
+        r=r+1/(b+a);
+        c=a;
+        a=a+b;
+        b=c;
+    end
 
 end
