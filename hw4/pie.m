3613fc27cfdac61ad90049595caf32cfb726df8d
Michael Shemesh <mshem@brandeis.edu>
2015-03-17 12:52:00 -0400

    Update pie.m

--- hw4/pie.m
+++ hw4/pie.m
@@ -1,8 +1,10 @@
-function p = pie(n)
-p=0;
-for i=1:n
-  p=p+1/(i^2);
-end
+% Author: Michael Shemesh
 
-p=sqrt(6*p);
+function p = pie(n)
+  p=0;
+  for i=1:n
+  % power series of pi
+    p=p+1/(i^2);
+  end
+  p=sqrt(6*p);
 end
