bd0767792a76db2461446b939204813e333402e1
Michael Shemesh <mshem@brandeis.edu>
2015-03-17 12:56:09 -0400

    Update taylorcos.m

--- hw4/taylorcos.m
+++ hw4/taylorcos.m
@@ -1,16 +1,16 @@
+% Author: Michael Shemesh
+
 function t = taylorcos(x)
-x=x*pi/180; %this expansion only works for radians so we must convert the input, which is degrees, into radians
-E=1;
-n=1;
-result=1;
-temp=0;
-while E>0.000001
-    temp=result;
-    result=result+((((-1)^(n))*(x^(2*n)))/(factorial(2*n)));
-
-    E=abs((result-temp)/temp);
-    n=n+1;
-end
-t=result;
-
+    x=x*pi/180; % this expansion only works for radians so we must convert the input, which is degrees, into radians
+    E=1;
+    n=1;
+    result=1;
+    temp=0;
+    while E>0.000001 % while our error term is above a threshold
+        temp=result;
+        result=result+((((-1)^(n))*(x^(2*n)))/(factorial(2*n))); % the series
+        E=abs((result-temp)/temp);
+        n=n+1;
+    end
+    t=result;
 end
