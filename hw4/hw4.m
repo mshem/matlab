1d1766f827563475d01bedea724f8d7a505fcb36
Michael Shemesh <mshem@brandeis.edu>
2015-03-17 12:49:47 -0400

    added comments

--- hw4/hw4.m
+++ hw4/hw4.m
@@ -1,9 +1,13 @@
+% Author: Michael Shemesh
+
 format long
+%Problem 1
 a=taylorcos(35)
 error=cos(35*pi/180)-a
 b=taylorsin(125)
 error=sin(125*pi/180)-b
 
+%Problem 2
 c=pie(100)
 error=pi-c
 d=pie(10000)
@@ -11,6 +15,7 @@ error=pi-d
 e=pie(1000000)
 error=pi-e
 
+%Problem 3
 f=reciprofibo(10)
 g=reciprofibo(50)
 h=reciprofibo(100)
